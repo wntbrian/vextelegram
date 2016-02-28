a42a30c709999ebc766dda7b629bd01b7cc9a606
svladimirov <ivc.vladimirovsa@gmail.com>
2016-02-28 15:52:23 +1000

    help

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 var TelegramBot = require('node-telegram-bot-api');
 var MongoClient = require('mongodb').MongoClient
-  , format = require('util').format;
+    , format = require('util').format;
 var feed = require("feed-read");
 
 var menu = require("./json/menu.json");
@@ -9,11 +9,11 @@ var cont = require("./json/contacts.json");
 var token = process.env.bottoken;
 var mongourl = "mongodb://127.0.0.1:27017/vexbot";
 var options = {
-  webHook: {
-    port: 8080
-    //key: __dirname+'/key.pem',
-    //cert: __dirname+'/crt.pem'
-  }
+    webHook: {
+        port: 8080
+        //key: __dirname+'/key.pem',
+        //cert: __dirname+'/crt.pem'
+    }
 };
 
 require("./credits.js");
@@ -21,122 +21,134 @@ require("./deposits.js");
 require("./test.js");
 var news_json;
 var yt_json;
-require("./rssfeed.js").rss(cont.rss.youtube_channel,function (json,err) {
-  yt_json = json;
+require("./rssfeed.js").rss(cont.rss.youtube_channel, function (json, err) {
+    yt_json = json;
 });
-require("./rssfeed.js").rss(cont.rss.news_feed ,function (json,err) {
-  news_json = json;
+require("./rssfeed.js").rss(cont.rss.news_feed, function (json, err) {
+    news_json = json;
 });
 ///
 
 var g_dep_arr;
 var g_card_arr;
-require("./modules.js").findProducts("deposit" ,function (json) {
+require("./modules.js").findProducts("deposit", function (json) {
     g_dep_arr = json;
 });
-require("./modules.js").findProducts("credits" ,function (json) {
+require("./modules.js").findProducts("credits", function (json) {
     g_card_arr = json;
 });
 
 var twittermsg = "";
 var Twitter = require('twitter');
 var client = new Twitter({
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
-client.get('statuses/user_timeline', { screen_name : "vostbank", count : 10 }, function(error, tweets, response){
-  if(error) throw error;
-  twittermsg = tweets;
-  //console.log(response);  // Raw response object.
+client.get('statuses/user_timeline', {screen_name: "vostbank", count: 10}, function (error, tweets, response) {
+    if (error) throw error;
+    twittermsg = tweets;
+    //console.log(response);  // Raw response object.
 });
 ///
 
-function randomInt (low, high) {
-  return Math.floor(Math.random() * (high - low) + low);
+function randomInt(low, high) {
+    return Math.floor(Math.random() * (high - low) + low);
 }
 var bot = new TelegramBot(token, options);
-bot.setWebHook(process.env.webhookurl+"/"+token);
+bot.setWebHook(process.env.webhookurl + "/" + token);
 
 // Matches /echo [whatever]
 bot.onText(/\/echo (.+)/, function (msg, match) {
-  var fromId = msg.from.id;
-  var resp = match[1];
-  bot.sendMessage(fromId, resp);
+    var fromId = msg.from.id;
+    var resp = match[1];
+    bot.sendMessage(fromId, resp);
 });
 
 bot.on('message', function (msg) {
-  var chatId = msg.chat.id;
-  var commands = require("./json/commands.json");
-  if ( typeof msg.reply_to_message == "undefined") {
-    if (typeof msg.text !== "undefined") {
-      switch (msg.text) {
-        case commands.start:
-          vb_start(msg);
-          break;
-          case commands.setplace:
-              vb_saveuserplace(msg);
-              break;
-        case commands.menu:
-          vb_menu(msg);
-          break;
-        case commands.atm:
-          vb_near(msg,"atm");
-          break;
-        case commands.office:
-          vb_near(msg,"office");
-          break;
-        case commands.contact:
-          vb_contacts(msg);
-          break;
-        case commands.sms:
-          vb_sms(msg);
-          break;
-        case commands.news:
-            vb_random_news(msg);
-            break;
-        case commands.youtube:
-          vb_youtube(msg);
-          break;
-        case commands.twitter:
-          vb_twitter(msg);
-          break;
-        case commands.curs:
-            //TODO курсы ЦБ http://cbr.ru/
-          vb_curs3(msg);
-          break;
-        case commands.products:
-          vb_products(msg);
-          break;
-        case commands.cr_card:
-          vb_credit_cards(msg);
-          break;
-        case commands.deposit:
-          vb_deposits2(msg);
-          break;
-        case commands.bonus:
-          vb_bonus(msg);
-          break;
-        default:
-          bot.sendMessage(chatId, "Для открытия стартового меню наберите /start");
-      }
+    var chatId = msg.chat.id;
+    var commands = require("./json/commands.json");
+    if (typeof msg.reply_to_message == "undefined") {
+        if (typeof msg.text !== "undefined") {
+            switch (msg.text) {
+                case commands.start:
+                    vb_start(msg);
+                    break;
+                case commands.help:
+                    vb_help(msg);
+                    break;
+                case commands.setplace:
+                    vb_saveuserplace(msg);
+                    break;
+                case commands.menu:
+                    vb_menu(msg);
+                    break;
+                case commands.atm:
+                    vb_near(msg, "atm");
+                    break;
+                case commands.office:
+                    vb_near(msg, "office");
+                    break;
+                case commands.contact:
+                    vb_contacts(msg);
+                    break;
+                case commands.sms:
+                    vb_sms(msg);
+                    break;
+                case commands.news:
+                    vb_random_news(msg);
+                    break;
+                case commands.youtube:
+                    vb_youtube(msg);
+                    break;
+                case commands.twitter:
+                    vb_twitter(msg);
+                    break;
+                case commands.curs:
+                    //TODO курсы ЦБ http://cbr.ru/
+                    vb_curs3(msg);
+                    break;
+                case commands.products:
+                    vb_products(msg);
+                    break;
+                case commands.cr_card:
+                    vb_credit_cards(msg);
+                    break;
+                case commands.deposit:
+                    vb_deposits2(msg);
+                    break;
+                case commands.bonus:
+                    vb_bonus(msg);
+                    break;
+                default:
+                    bot.sendMessage(chatId, "Для открытия стартового меню наберите /start");
+            }
+        }
     }
-  }
     // photo can be: a file path, a stream or a Telegram file_id
     //var photo = 'cat.jpg';
 
     //bot.sendPhoto(chatId, photo, {caption: 'Lovely kittens'});
 });
 
-function vb_start(msg){
-    var resp = "*Вас приветствует тестовый БОТ банка «Восточный»*\n"+
+function vb_start(msg) {
+    var resp = "*Вас приветствует тестовый БОТ банка «Восточный»*\n" +
         "Я помогу Вам получить актуальную информацию о наших продуктах и услугах," +
         " а также расскажу о самых свежих новостях 😊";
-    bot.sendMessage(msg.from.id,resp,menu.main);
+    bot.sendMessage(msg.from.id, resp, menu.main);
 };
 
-function vb_saveuserplace(msg){
+function vb_help(msg) {
+    var help_list = require("./json/help.json");
+    var resp = "*Список доступных команд*:\n\n";
+    for (i in help_list) {
+        resp += help_list[i].name + " - " + help_list[i].desc+"\n";
+    }
+    bot.sendMessage(msg.from.id, resp, menu.main);
+};
+
+function vb_saveuserplace(msg) {
     bot.sendMessage(msg.from.id, 'Введите наименование населенного пункта', menu.reply).then(function (sended) {
         var chatId = sended.chat.id;
         var messageId = sended.message_id;
@@ -156,31 +168,34 @@ function vb_saveuserplace(msg){
     })
 };
 
-function vb_menu(msg){
+function vb_menu(msg) {
     var resp = "Пожалуйста, выберите пункт меню";
-    bot.sendMessage(msg.from.id,resp,menu.main);
+    bot.sendMessage(msg.from.id, resp, menu.main);
 }
 
-function vb_contacts(msg){
+function vb_contacts(msg) {
     var resp = "";
-    for (var i in cont.telephones ){
+    for (var i in cont.telephones) {
         resp += cont.telephones[i].title + " " + cont.telephones[i].numb + "\n";
-    };
+    }
+    ;
     resp += "\n";
-    for (var i in cont.main_url ){
+    for (var i in cont.main_url) {
         resp += "• [" + cont.main_url[i].title + "](" + cont.main_url[i].link + ")\n";
-    };
+    }
+    ;
     resp += "\n*«Восточный» в социальных сетях*\n"
-    for (var i in cont.social_url ){
+    for (var i in cont.social_url) {
         resp += "• [" + cont.social_url[i].title + "](" + cont.social_url[i].link + ")\n";
-    };
-    bot.sendMessage(msg.from.id,resp,menu.main);
+    }
+    ;
+    bot.sendMessage(msg.from.id, resp, menu.main);
 }
 
-function vb_products(msg){
+function vb_products(msg) {
     var fromId = msg.from.id;
     var resp = "Что вас интересует?";
-    bot.sendMessage(fromId,resp,menu.products);
+    bot.sendMessage(fromId, resp, menu.products);
 }
 
 function vb_credit_cards(msg) {
@@ -197,20 +212,19 @@ function vb_credit_cards(msg) {
 };
 
 function vb_deposits(msg) {
-    findProducts("deposit",function(result) {
-        var resp = "";
-        if (result.length) {
-            for (var atr in result) {
-                resp += "[Вклад " + result[atr].title.trim() + "](" + cont.bank_khb + result[atr].link.trim() + ")\n";
-            }
-            bot.sendMessage(msg.from.id, resp, menu.products)
+    var resp = "";
+    if (g_dep_arr.length) {
+        for (var atr in result) {
+            resp += "[Вклад " + g_dep_arr[atr].title.trim() + "](" + cont.bank_khb + g_dep_arr[atr].link.trim() + ")\n";
         }
-        else {
-            bot.sendMessage(msg.from.id, "По вашему запросу ничего не найдено :-(", menu.products)
-        }
-    });
+        bot.sendMessage(msg.from.id, resp, menu.products)
+    }
+    else {
+        bot.sendMessage(msg.from.id, "По вашему запросу ничего не найдено :-(", menu.products)
+    }
 }
 
+//TODO выбор продукта
 function vb_deposits2(msg) {
     var resp = "";
     if (g_dep_arr.length) {
@@ -223,13 +237,13 @@ function vb_deposits2(msg) {
                 var chatId = sended.chat.id;
                 var messageId = sended.message_id;
                 bot.onReplyToMessage(chatId, messageId, function (message) {
-                    console.log("message1:"+message.text);
+                    console.log("message1:" + message.text);
                     if (typeof message.text !== "undefined") {
-                        console.log("result:"+g_dep_arr.toString());
+                        console.log("result:" + g_dep_arr.toString());
                         for (var atr in g_dep_arr) {
-                            console.log("message2:"+message.text);
-                            if (message.text = atr){
-                                bot.sendMessage(msg.from.id, "Информация по вкладу "+g_dep_arr[atr].title.trim(), menu.products)
+                            console.log("message2:" + message.text);
+                            if (message.text = atr) {
+                                bot.sendMessage(msg.from.id, "Информация по вкладу " + g_dep_arr[atr].title.trim(), menu.products)
                             }
                         }
                     }
@@ -242,27 +256,30 @@ function vb_deposits2(msg) {
     }
 };
 
-function vb_bonus(msg){
-  var bonuses = require("./json/bonus.json");
-  var resp = "";
-  for (var atr in bonuses){
-        resp += "["+bonuses[atr].title+"]("+bonuses[atr].link+")\n"+bonuses[atr].desc+"\n";
-    };
-  bot.sendMessage(msg.from.id,resp,menu.none);
+function vb_bonus(msg) {
+    var bonuses = require("./json/bonus.json");
+    var resp = "";
+    for (var atr in bonuses) {
+        resp += "[" + bonuses[atr].title + "](" + bonuses[atr].link + ")\n" + bonuses[atr].desc + "\n";
+    }
+    ;
+    bot.sendMessage(msg.from.id, resp, menu.none);
 }
 
-function vb_sms(msg){
+function vb_sms(msg) {
     var sms_codes = require("./json/sms_codes.json");
     var resp = "";
     sms_codes.info;
-    for (var i in sms_codes.title ){
+    for (var i in sms_codes.title) {
         resp += sms_codes.title[i] + "\n";
-    };
+    }
+    ;
     resp += "\n";
-    for (var i in sms_codes.codes ){
+    for (var i in sms_codes.codes) {
         resp += "• " + sms_codes.codes[i].title + "\n  " + sms_codes.codes[i].text + "\n";
-    };
-    bot.sendMessage(msg.from.id,resp,menu.none);
+    }
+    ;
+    bot.sendMessage(msg.from.id, resp, menu.none);
 }
 
 function vb_near(msg, p_type) {
@@ -275,9 +292,10 @@ function vb_near(msg, p_type) {
                 var tmp_loc;
                 if (typeof message.location == "undefined") {
                     tmp_loc = [30.35515, 59.91884];
-                };
+                }
+                ;
                 findNear(tmp_loc, p_type, function (err, loc) {
-                    if (err){
+                    if (err) {
                         console.log(err);
                     }
                     else {
@@ -290,15 +308,17 @@ function vb_near(msg, p_type) {
                                 bot.sendLocation(fromId, loc.coordX, loc.coordY, menu.main);
                                 break;
                             case "office":
-                                parse_office_desc(loc.desc, function(txt) {
+                                parse_office_desc(loc.desc, function (txt) {
                                     txt = "*Ближайшее отделение находится в " + loc.distance + " метрах*\n" +
                                         "*Режим работы:*" + txt;
                                     bot.sendMessage(fromId, txt, menu.main);
                                     bot.sendLocation(fromId, loc.coordX, loc.coordY, menu.main);
                                 });
                                 break;
-                        };
-                     };
+                        }
+                        ;
+                    }
+                    ;
                 });
             });
         });
@@ -309,13 +329,13 @@ var parse_office_desc = function (desc, callback) {
     var str_arr = desc.split(",");
     var ret = "";
     for (i in str_arr) {
-        ret += str_arr[i].replace("Касса:","\n*Касса*\n").replace("Перерыв:","\n*Перерыв*\n").replace("Отделение:","\n")+"\n";
+        ret += str_arr[i].replace("Касса:", "\n*Касса*\n").replace("Перерыв:", "\n*Перерыв*\n").replace("Отделение:", "\n") + "\n";
     }
     callback(ret);
 }
 
 function vb_random_news(msg) {
-    switch (randomInt(0,9)) {
+    switch (randomInt(0, 9)) {
         case 0:
         case 1:
         case 2:
@@ -331,30 +351,31 @@ function vb_random_news(msg) {
         case 8:
         default:
             vb_news(msg);
-    };
+    }
+    ;
 };
 
-function vb_twitter(msg){
-  var resp = "twitter";
-  resp = twittermsg[randomInt(0,10)].text;
-  bot.sendMessage(msg.from.id,resp,menu.main);
+function vb_twitter(msg) {
+    var resp = "twitter";
+    resp = twittermsg[randomInt(0, 10)].text;
+    bot.sendMessage(msg.from.id, resp, menu.main);
 }
 
-function vb_news(msg){
-  var resp = "новости";
-  var i = randomInt(0,news_json.length-1);
-  resp = news_json[i].title+"\n"+news_json[i].link;
-  bot.sendMessage(msg.from.id,resp,menu.main);
+function vb_news(msg) {
+    var resp = "новости";
+    var i = randomInt(0, news_json.length - 1);
+    resp = news_json[i].title + "\n" + news_json[i].link;
+    bot.sendMessage(msg.from.id, resp, menu.main);
 }
 
-function vb_youtube(msg){
+function vb_youtube(msg) {
     var resp = "youtube";
-    var i = randomInt(0,yt_json.length-1);
-    resp = yt_json[i].title+"\n"+yt_json[i].link;
-    bot.sendMessage(msg.from.id,resp,menu.main);
+    var i = randomInt(0, yt_json.length - 1);
+    resp = yt_json[i].title + "\n" + yt_json[i].link;
+    bot.sendMessage(msg.from.id, resp, menu.main);
 }
 
-var findNear = function(coord, p_type, callback) {
+var findNear = function (coord, p_type, callback) {
     var resp;
     if (typeof coord == "undefined") {
         callback("Не переданы координаты", resp);
@@ -389,28 +410,31 @@ var findNear = function(coord, p_type, callback) {
                     }
                     else {
                         if (result.length) {
-                            resp = {"desc": result[0].desc,
-                                    "distance": result[0].distance.toFixed(0),
-                                    "coordX": result[0].loc.coordinates[1],
-                                    "coordY": result[0].loc.coordinates[0]};
+                            resp = {
+                                "desc": result[0].desc,
+                                "distance": result[0].distance.toFixed(0),
+                                "coordX": result[0].loc.coordinates[1],
+                                "coordY": result[0].loc.coordinates[0]
+                            };
                         }
                         else {
                             resp = {"desc": "К сожалению, поблизости ничего не найдено"};
                         }
                         console.log(resp);
                         db.close();
-                        callback("",resp);
-                    };
+                        callback("", resp);
+                    }
+                    ;
                 });
             }
         });
-   }
+    }
 };
 
 function vb_curs3(msg) {
     var fromId = msg.from.id;
     var curr = require("./json/currency.json");
-    require("./modules.js").GetUserUrl(msg.from.id, function(url) {
+    require("./modules.js").GetUserUrl(msg.from.id, function (url) {
         console.log(url);
         require("./parse_curs.js").get_curs(url, function (err, curs_json) {
             if (err) {
@@ -431,11 +455,11 @@ function vb_curs3(msg) {
 };
 
 //TODO найти решения для таблицы
-function vb_table(msg){
+function vb_table(msg) {
     var fromId = msg.from.id;
     var resp = "Проверка\n" +
-        "```раз два три четыре ```\n"+
-        "```пять шесть семь```"+
+        "```раз два три четыре ```\n" +
+        "```пять шесть семь```" +
         "`djçlkç`";
-    bot.sendMessage(fromId,resp,menu.main);
+    bot.sendMessage(fromId, resp, menu.main);
 };
