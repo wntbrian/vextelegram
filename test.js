a27971272ccf28ba03b80152804a58631e2b0bd8
svladimirov <svladimirov@example.com>
2016-02-29 11:13:22 +1000

    курс цб

--- test.js
+++ test.js
@@ -1,12 +1,12 @@
-//var cont = require("./json/contacts.json");
-//console.log("begin test");
-//require("./parse_curs.js").get_curs("http://www.vostbank.ru/khabarovsk" ,function (json) {
-//    console.log("JSON : " + json);
-//    //var curs = JSON.parse(json);
-//    for (var i in json) {
-//        console.log(json[i].name);
-//        console.log(json[i].buy);
-//        console.log(json[i].sell);
-//    }
-//    console.log("end test");
-//});
+
+
+var request = require('request');
+var url = 'http://query.yahooapis.com/v1/public/yql?q=select+*+from+yahoo.finance.xchange+where+pair+=+%22USDRUB,EURRUB%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=';
+request(url, function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    var fbResponse = JSON.parse(body);
+    console.log("Got a response: ", fbResponse);
+  } else {
+    console.log("Got an error: ", error, ", status code: ", response.statusCode);
+  }
+});
